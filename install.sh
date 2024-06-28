e6b02cccdacba432709b6938aaa36004ff641bd2
Georgi Knox <gknox@slack-corp.com>
2024-06-28 13:05:52 -0400

    already installed checks

--- install.sh
+++ install.sh
@@ -1,21 +1,75 @@
-# git
-git config --global user.name "Georgi Knox"
-git config --global user.email "georgicodes@github.com"
-git config --global core.editor vim
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
+#!/bin/bash
 
-# vim
-mkdir -p ~/.vim/colors
-cp ~/.dotfiles/.vim/colors/heman.vim ~/.vim/colors/
-ln -s ~/.dotfiles/.zshrc ~/
-ln -s ~/.dotfiles/.vimrc ~/
+os=$(uname) # Linux|Darwin
 
+# install homebrew
+if [[ $os == "Darwin" ]]; then
+    if ! command -v brew &>/dev/null; then
+        echo "Homebrew not found. Installing..."
+        # Use curl to download the installation script
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "Homebrew already installed."
+    fi
+fi
 
-# zsh 
-if [ "$SHELL" != "/usr/bin/zsh" ]; then
-    sudo apt install -y zsh
+# install zsh
+if command -v zsh &>/dev/null; then
+    echo "zsh already installed."
+else
+    echo "zsh is not installed, fixing that now"
+    if [[ $os == "Darwin" ]]; then
+        brew install zsh
+    else
+        sudo apt install -y zsh
+    fi
+    source ~/.zshrc
     zsh
-fi;
+fi
+
+# install oh-my-zsh + plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+export ZSH_CUSTOM=~/.oh-my-zsh/custom
+echo "copying os specific zsh configs to $ZSH_CUSTOM folder"
+if [[ $os == "Darwin" ]]; then
+    cp macos.zsh "$ZSH_CUSTOM"
+    echo "Copied macos.zsh to $ZSH_CUSTOM"
+else
+    cp linux.zsh "$ZSH_CUSTOM"
+    echo "Copied linux.zsh to $ZSH_CUSTOM"
+fi
+
+if [[ $os == "Darwin" ]]; then
+
+    if ! brew list nvm &>/dev/null; then
+        echo "nvm is not installed. Installing..."
+        # Install nvm using Homebrew
+        echo "installing nvm + node"
+        brew install nvm
+        nvm install node
+
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"                                       # This loads nvm
+        [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_com
+    else
+        echo "zsh already installed."
+    fi
+else
+    echo "not installing nvm + node on Linux rn"
+fi
+
+if [[ $os == "Darwin" ]]; then
+    if ! brew list git &>/dev/null; then
+        echo "git is not installed. Installing..."
+        brew install git
+        cp .gitconfig ~/.gitconfig
+    else
+        echo "git already installed."
+    fi
+else
+    sudo apt install git
+    cp .gitconfig ~/.gitconfig
+fi
---
+++ macos.zsh
@@ -0,0 +1,2 @@
+export $OMP_HOME="~/dev/oh-my-posh/themes/"
+eval "$(oh-my-posh init zsh --config $OMP_HOME/M365Princess.omp.json)"
